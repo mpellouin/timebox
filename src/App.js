73d8203ab1a00f46eecde5303ab95708a2c23292
mpellouin <maxence.pellouin@epitech.eu>
2023-12-08 15:02:40 +0900

    fix(layout): got rid of custom UI to be able to use shapes and text

--- src/App.js
+++ src/App.js
@@ -1,89 +1,14 @@
-import { Canvas, Tldraw, track, useEditor } from '@tldraw/tldraw';
+import { Canvas, Tldraw } from '@tldraw/tldraw';
 import '@tldraw/tldraw/tldraw.css'
-import { useEffect } from 'react';
 import './App.css'
 import { Timebox } from './Timebox';
-
-const CustomUI = track(() => {
-	const editor = useEditor()
-
-	useEffect(() => {
-    editor.updateInstanceState({ canMoveCamera: false });
-		const handleKeyUp = (e) => {
-			switch (e.key) {
-				case 'Delete':
-				case 'Backspace': {
-					editor.deleteShapes(editor.selectedShapeIds)
-					break
-				}
-				case 'v': {
-					editor.setCurrentTool('select')
-					break
-				}
-				case 'e': {
-					editor.setCurrentTool('eraser')
-					break
-				}
-				case 'x':
-				case 'p':
-				case 'b':
-				case 'd': {
-					editor.setCurrentTool('draw')
-					break
-				}
-        case 'z':
-          if (e.ctrlKey) {
-            editor.undo()
-          }
-          break;
-        default:
-          
-          break;
-			}
-		}
-
-		window.addEventListener('keyup', handleKeyUp)
-		return () => {
-			window.removeEventListener('keyup', handleKeyUp)
-		}
-	})
-
-	return (
-		<div className="custom-layout">
-			<div className="custom-toolbar">
-				<button
-					className="custom-button"
-					data-isactive={editor.currentToolId === 'select'}
-					onClick={() => editor.setCurrentTool('select')}
-				>
-					Select
-				</button>
-				<button
-					className="custom-button"
-					data-isactive={editor.currentToolId === 'draw'}
-					onClick={() => editor.setCurrentTool('draw')}
-				>
-					Pencil
-				</button>
-				<button
-					className="custom-button"
-					data-isactive={editor.currentToolId === 'eraser'}
-					onClick={() => editor.setCurrentTool('eraser')}
-				>
-					Eraser
-				</button>
-			</div>
-		</div>
-	)
-})
 
 function App() {
 
   return (
     <div style={{ position: 'fixed', inset: 0 }}>
-      <Tldraw hideUi>
+      <Tldraw>
         <Canvas />
-        <CustomUI />
         <Timebox />
       </Tldraw>
     </div>
--- src/Timebox.js
+++ src/Timebox.js
@@ -1,5 +1,13 @@
-export const Timebox = () => {
+import { track, useEditor } from "@tldraw/tldraw";
+import { useEffect } from "react";
 
+export const Timebox = track(() => {
+	const editor = useEditor()
+
+
+  useEffect(() => {
+    editor.updateInstanceState({ canMoveCamera: false });
+  }, [editor])
     const hours = [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   return (
@@ -35,13 +43,15 @@ export const Timebox = () => {
             </div>
             <table className="spaceTop">
                 <thead>
+                  <tr>
                     <td className="firstCol header"></td>
                     <td className="secondCol header">:00</td>
                     <td className="thirdCol header">:30</td>
+                    </tr>
                 </thead>
                 <tbody>
-                {hours.map(hour => (
-                    <tr>
+                {hours.map((hour, index) => (
+                    <tr key={`${hour}-${index}`}>
                         <td className="firstCol">{hour}</td>
                         <td className="secondCol"></td>
                         <td className="thirdCol"></td>
@@ -54,4 +64,4 @@ export const Timebox = () => {
       </div>
     </div>
   );
-};
+});
